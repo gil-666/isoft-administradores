4e14737158d5edff177f04bd9ee7367c1683a025
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-11-07 16:15:17 -0600

    mounted

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import '../Controller';
 import { autenticarUser } from '../Controller';
