377cbf35fe9297960dd8498e8a0eda3b719338aa
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-11-28 19:23:21 -0600

    arreglo

--- src/components/elements/Mapa.vue
+++ src/components/elements/Mapa.vue
@@ -124,7 +124,7 @@ export default {
         this.currentDestination = this.getClosestMarker();
         this.routeStatus = `Siguiente destino: ${this.currentDestination.direccion_completa}`;
         this.buttonText = "Siguiente destino";
-        this.speak(this.routeStatus); // Texto a voz
+        // this.speak(this.routeStatus); // Texto a voz
         this.drawRoute();
       }
     },
