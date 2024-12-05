1d6c51c5a88081ff63873df91c5e43ca159e81e2
gil-666 <143546119+gil-666@users.noreply.github.com>
2024-12-04 19:34:52 -0600

    import usuarioactual

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { usuarioActual } from '@/main';
 
 const router = useRouter();
 const isLoaded = ref(false);
